338aba3c489155a14f419b6d8a57247ab8fe3566
deepakanroman <deepakkumar231220033@gmail.com>
2025-02-16 09:47:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        HELLO WORLD!<br />
+          Task 6: Basic React Application Development <br />
+          This is my first React App. <br />
+          I am so excited to learn React!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
